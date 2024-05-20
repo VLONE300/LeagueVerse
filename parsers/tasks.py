cd5776093f1632ed3a4f9d3ea4c2e283d56420dc
vlone <ermolich158@gmail.com>
2024-05-20 12:50:02 +0300

    changed standings model and changed parsing standings

--- parsers/tasks.py
+++ parsers/tasks.py
@@ -1,12 +1,12 @@
 from celery import shared_task
 
 from leagues.models import NBAStandings, NBATeams
-from parsers.utils import parsing_nba_standings
+from parsers.utils import scrape_standings
 
 
 @shared_task
 def save_nba_standings():
-    for data in parsing_nba_standings():
+    for data in scrape_standings():
         standings_instance, created = NBAStandings.objects.update_or_create(
             team_id=NBATeams.objects.get(name=data['team_name']).id,
             defaults={
@@ -15,12 +15,6 @@ def save_nba_standings():
                 'winning_percentage': data['winrate'],
                 'games_back': data['gb'],
                 'points_percentage_game': data['points'],
-                'oop_points_percentage_game': data['opp_points'],
-                'home_record': data['home'],
-                'away_record': data['away'],
-                'conference_record': data['conf'],
-                'division_record': data['div'],
-                'last_ten_games': data['L10'],
-                'streak': data['streak']
+                'oop_points_percentage_game': data['opp_points']
             }
         )
