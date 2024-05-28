381817da274a22790cc0d5655bd1d8ddc8148fd4
vlone <ermolich158@gmail.com>
2024-05-28 15:11:31 +0300

    follow to league

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path, include
 
-from users.views import ActivateUser
+from users.views import ActivateUser, AddFavoriteLeagueView
 
 urlpatterns = [
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.jwt')),
     path('activate/<str:uid>/<str:token>/', ActivateUser.as_view({'get': 'activation'}), name='activate'),
+    path('add-favorite-league/<int:league_id>/', AddFavoriteLeagueView.as_view(), name='add_favorite_league'),
+    path('favorite-leagues/', AddFavoriteLeagueView.as_view(), name='favorite_leagues'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,4 +1,11 @@
 from djoser.views import UserViewSet
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from core.models import League
+from core.serializers import LeagueSerializer
 
 
 class ActivateUser(UserViewSet):
@@ -12,3 +19,24 @@ class ActivateUser(UserViewSet):
         }
 
         return serializer_class(*args, **kwargs)
+
+
+class AddFavoriteLeagueView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, league_id):
+        user = request.user
+        try:
+            league = League.objects.get(id=league_id)
+        except League.DoesNotExist:
+            return Response({'detail': 'League not found.'}, status=status.HTTP_404_NOT_FOUND)
+
+        user.fav_league.add(league)
+        user.save()
+        return Response({'detail': 'League added to favorites.'}, status=status.HTTP_200_OK)
+
+    def get(self, request):
+        user = request.user
+        favorite_leagues = user.fav_league.all()
+        serializer = LeagueSerializer(favorite_leagues, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
