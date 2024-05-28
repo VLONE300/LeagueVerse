fb7bffd81aa94f56aadc744bdbccd085265e29ce
vlone <ermolich158@gmail.com>
2024-05-28 17:06:05 +0300

    follow to league

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 
-from users.views import ActivateUser, AddFavoriteLeagueView
+from users.views import ActivateUser, AddFavoriteLeagueView, AddFavoriteTeamView
 
 urlpatterns = [
     path('auth/', include('djoser.urls')),
@@ -8,4 +8,5 @@ urlpatterns = [
     path('activate/<str:uid>/<str:token>/', ActivateUser.as_view({'get': 'activation'}), name='activate'),
     path('add-favorite-league/<int:league_id>/', AddFavoriteLeagueView.as_view(), name='add_favorite_league'),
     path('favorite-leagues/', AddFavoriteLeagueView.as_view(), name='favorite_leagues'),
+    path('add-favorite-team/<str:team_type>/<int:team_id>/', AddFavoriteTeamView.as_view(), name='add_favorite_team'),
 ]
