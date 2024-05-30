402234f6a82c4edfe8f04972a820ccd80b853f92
vlone <ermolich158@gmail.com>
2024-05-30 15:06:52 +0300

    nba schedule endpoints

--- nba/urls.py
+++ nba/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from rest_framework import routers
 
-from nba.views import NBATeamsView, NBAStandingsView, NBAGameView
+from nba.views import NBATeamsView, NBAStandingsView, NBAScoreView, NBAScheduleView
 
 router = routers.SimpleRouter()
 router.register(r'teams', NBATeamsView)
-router.register(r'scores', NBAGameView)
+router.register(r'scores', NBAScoreView)
+router.register(r'schedule', NBAScheduleView, basename='schedule')
 
 urlpatterns = [
     path('standings/', NBAStandingsView.as_view(), name='nba_standings'),
