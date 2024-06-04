b167cb122addb2a512f3bc8ab443e4269faab685
vlone <ermolich158@gmail.com>
2024-06-04 12:53:24 +0300

    remove dublicate code from views

--- nba/views.py
+++ nba/views.py
@@ -1,7 +1,5 @@
-from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework.response import Response
 from rest_framework.viewsets import ReadOnlyModelViewSet
-
+from core.views import StandingsView, GamesView
 from nba.models import NBAStanding, NBATeam, NBAGame
 from nba.serializers import NBATeamsSerializer, NBAStandingsSerializer, NBAGamesSerializer
 
@@ -11,36 +9,16 @@ class NBATeamsView(ReadOnlyModelViewSet):
     serializer_class = NBATeamsSerializer
 
 
-class NBAStandingsView(ReadOnlyModelViewSet):
+class NBAStandingsView(StandingsView):
     serializer_class = NBAStandingsSerializer
     queryset = NBAStanding.objects.all()
 
-    def list(self, request, *args, **kwargs):
-        queryset = self.get_queryset()
-        ordering = self.request.query_params.get('ordering', None)
-        if ordering:
-            queryset = queryset.order_by(ordering)
 
-        data = {}
-        for standing in queryset:
-            conference = standing.team.conference
-            if conference not in data:
-                data[conference] = []
-            serializer = self.get_serializer(standing)
-            data[conference].append(serializer.data)
-
-        return Response(data)
-
-
-class NBAScoreView(ReadOnlyModelViewSet):
+class NBAScoreView(GamesView):
     queryset = NBAGame.objects.filter(status='Finished').order_by('-date')
     serializer_class = NBAGamesSerializer
-    filter_backends = [DjangoFilterBackend]
-    filterset_fields = ['date']
 
 
-class NBAScheduleView(ReadOnlyModelViewSet):
+class NBAScheduleView(GamesView):
     queryset = NBAGame.objects.filter(status='Waiting').order_by('-date')
     serializer_class = NBAGamesSerializer
-    filter_backends = [DjangoFilterBackend]
-    filterset_fields = ['date']
