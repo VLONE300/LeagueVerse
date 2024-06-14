05e65757ba4c72d4330a5dbe82a36debfcf8f4b6
vlone <ermolich158@gmail.com>
2024-06-14 13:24:01 +0300

    add models for nba_box_score

--- nba/models.py
+++ nba/models.py
@@ -14,7 +14,7 @@ class NBATeam(Team):
 
 
 class NBAStanding(ConferenceStanding):
-    team = models.ForeignKey(NBATeam, on_delete=models.CASCADE)
+    team = models.ForeignKey("NBATeam", on_delete=models.CASCADE)
     winning_percentage = models.FloatField()
     games_back = models.CharField(max_length=10)
     points_percentage_game = models.FloatField()
@@ -22,8 +22,8 @@ class NBAStanding(ConferenceStanding):
 
 
 class NBAGame(Game):
-    visitor_team = models.ForeignKey(NBATeam, on_delete=models.CASCADE, related_name='visitor_games')
-    home_team = models.ForeignKey(NBATeam, on_delete=models.CASCADE, related_name='home_games')
+    visitor_team = models.ForeignKey("NBATeam", on_delete=models.CASCADE, related_name='visitor_games')
+    home_team = models.ForeignKey("NBATeam", on_delete=models.CASCADE, related_name='home_games')
 
     class Meta:
         verbose_name = "NBA Game"
@@ -31,3 +31,28 @@ class NBAGame(Game):
 
     def __str__(self):
         return f'{self.date} {self.visitor_team} - {self.home_team}'
+
+
+class NBABoxScore(models.Model):
+    game = models.OneToOneField("NBAGame", on_delete=models.CASCADE)
+    home_team_stats = models.ForeignKey("NBATeamStats", on_delete=models.CASCADE, related_name='home_box_scores')
+    visitor_team_stats = models.ForeignKey("NBATeamStats", on_delete=models.CASCADE, related_name='visitor_box_scores')
+
+
+class NBATeamStats(models.Model):
+    field_goals = models.IntegerField(default=0)
+    field_goal_attempts = models.IntegerField(default=0)
+    field_goals_percentage = models.FloatField(default=0.0)
+    three_point_field_goals = models.IntegerField(default=0)
+    three_point_field_goal_attempts = models.IntegerField(default=0)
+    three_point_field_goals_percentage = models.FloatField(default=0.0)
+    free_throws = models.IntegerField(default=0)
+    free_throw_attempts = models.IntegerField(default=0)
+    free_throw_percentage = models.FloatField(default=0.0)
+    personal_fouls = models.IntegerField(default=0)
+    total_rebounds = models.IntegerField(default=0)
+    offensive_rebounds = models.IntegerField(default=0)
+    turnovers = models.IntegerField(default=0)
+    assists = models.IntegerField(default=0)
+    steals = models.IntegerField(default=0)
+    blocks = models.IntegerField(default=0)
