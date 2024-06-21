890371d5b8af6f698506ae3c137ada8383c4480f
vlone <ermolich158@gmail.com>
2024-06-21 11:59:14 +0300

    models for nhl box score

--- nhl/models.py
+++ nhl/models.py
@@ -25,6 +25,7 @@ class NHLGame(Game):
     visitor_team = models.ForeignKey(NHLTeam, on_delete=models.CASCADE, related_name='visitor_games')
     home_team = models.ForeignKey(NHLTeam, on_delete=models.CASCADE, related_name='home_games')
     overtime = models.CharField(max_length=10, blank=True, null=True)
+    box_score = models.IntegerField(blank=True, null=True)
 
     class Meta:
         verbose_name = "NHL Game"
@@ -32,3 +33,18 @@ class NHLGame(Game):
 
     def __str__(self):
         return f'{self.date} {self.visitor_team} - {self.home_team}'
+
+
+class NHLBoxScore(models.Model):
+    home_team_stats = models.ForeignKey("NHLTeamStats", on_delete=models.CASCADE, related_name="home_team_stats")
+    visitor_team_stats = models.ForeignKey("NHLTeamStats", on_delete=models.CASCADE, related_name="visitor_team_stats")
+
+
+class NHLTeamStats(models.Model):
+    goals = models.IntegerField(blank=True, null=True)
+    assists = models.IntegerField(blank=True, null=True)
+    points = models.IntegerField(blank=True, null=True)
+    penalties_in_minutes = models.IntegerField(blank=True, null=True)
+    even_strength_goals = models.IntegerField(blank=True, null=True)
+    shots_on_goal = models.IntegerField(blank=True, null=True)
+    shooting_percentage = models.FloatField(blank=True, null=True)
