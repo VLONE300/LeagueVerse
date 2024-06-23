6ee202a66cd4e6c5fbec3fca7422a5917c43d8cd
vlone <ermolich158@gmail.com>
2024-06-23 15:44:28 +0300

    add endpoints for nhl team stats

--- nhl/urls.py
+++ nhl/urls.py
@@ -1,6 +1,6 @@
 from rest_framework import routers
 
-from nhl.views import NHLTeamsView, NHLStandingsView, NHLScoreView, NHLScheduleView, NHLGamesDateView
+from nhl.views import NHLTeamsView, NHLStandingsView, NHLScoreView, NHLScheduleView, NHLGamesDateView, NHLTeamStatsView
 
 router = routers.SimpleRouter()
 router.register(r'teams', NHLTeamsView)
@@ -8,6 +8,7 @@ router.register(r'standings', NHLStandingsView, basename='standings')
 router.register(r'scores', NHLScoreView, basename='scores')
 router.register(r'schedule', NHLScheduleView, basename='schedule')
 router.register(r'games-date', NHLGamesDateView, basename='games-date')
+router.register(r'stats', NHLTeamStatsView, basename='stats')
 
 urlpatterns = [
 ]
---
+++ nhl/utils.py
@@ -0,0 +1,94 @@
+from django.db.models import Sum
+from nhl.models import NHLGame
+from core.utils import get_games, calculate_stat
+
+
+def team_gpg(teams):
+    """Goals Per Game"""
+    return calculate_stat(NHLGame, teams, 'visitor_pts', 'home_pts')
+
+
+def team_ppg(teams):
+    """Power Play Goals"""
+    team_ppp_data = []
+    for team in teams:
+        visitor_games = get_games(NHLGame, team, is_home=False)
+        home_games = get_games(NHLGame, team, is_home=True)
+
+        visitor_power_play_goals = visitor_games.aggregate(
+            total=Sum('box_score__visitor_team_stats__power_play_goals'))['total']
+        home_power_play_goals = home_games.aggregate(
+            total=Sum('box_score__home_team_stats__power_play_goals'))['total']
+
+        total_power_play_goals = visitor_power_play_goals + home_power_play_goals
+
+        team_ppp_data.append({
+            'team': team.name,
+            'value': total_power_play_goals
+        })
+    return sorted(team_ppp_data, key=lambda x: x['value'], reverse=True)[:5]
+
+
+def team_pim(teams):
+    """Penalties Minutes"""
+    team_pim_data = []
+    for team in teams:
+        visitor_games = get_games(NHLGame, team, is_home=False)
+        home_games = get_games(NHLGame, team, is_home=True)
+
+        visitor_penalties_in_minutes = visitor_games.aggregate(
+            total=Sum('box_score__visitor_team_stats__penalties_in_minutes'))['total']
+        home_penalties_in_minutes = home_games.aggregate(
+            total=Sum('box_score__home_team_stats__penalties_in_minutes'))['total']
+
+        total_penalties_in_minutes = visitor_penalties_in_minutes + home_penalties_in_minutes
+
+        team_pim_data.append({
+            'team': team.name,
+            'value': round(total_penalties_in_minutes, 1)
+        })
+    return sorted(team_pim_data, key=lambda x: x['value'], reverse=True)[:5]
+
+
+def team_ast(teams):
+    """Assists made"""
+    return calculate_stat(NHLGame, teams, 'box_score__visitor_team_stats__assists',
+                          'box_score__home_team_stats__assists')
+
+
+def team_sog(teams):
+    """Shots On Goal"""
+    return calculate_stat(NHLGame, teams, 'box_score__visitor_team_stats__shots_on_goal',
+                          'box_score__home_team_stats__shots_on_goal')
+
+
+def team_shg(teams):
+    """Short Handed Goals"""
+    team_shg_data = []
+    for team in teams:
+        visitor_games = get_games(NHLGame, team, is_home=False)
+        home_games = get_games(NHLGame, team, is_home=True)
+
+        visitor_short_handed_goals = visitor_games.aggregate(
+            total=Sum('box_score__visitor_team_stats__short_handed_goals'))['total']
+        home_short_handed_goals = home_games.aggregate(
+            total=Sum('box_score__home_team_stats__short_handed_goals'))['total']
+
+        total_short_handed_goals = visitor_short_handed_goals + home_short_handed_goals
+
+        team_shg_data.append({
+            'team': team.name,
+            'value': total_short_handed_goals
+        })
+    return sorted(team_shg_data, key=lambda x: x['value'], reverse=True)[:5]
+
+
+def get_nhl_stats(teams):
+    return {
+        'Goals Per Game': team_gpg(teams),
+        'Assists': team_ast(teams),
+        'Shots': team_sog(teams),
+        'Power Play Goals': team_ppg(teams),
+        'Short Handed Goals': team_shg(teams),
+        'Penalties Minutes': team_pim(teams),
+    }
