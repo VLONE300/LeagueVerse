9d4303a491b4995ac42db0111b14180a9ceb75ec
vlone <ermolich158@gmail.com>
2024-06-27 22:43:25 +0300

    change celery beat

--- leagueverse/celery.py
+++ leagueverse/celery.py
@@ -12,18 +12,18 @@ app.autodiscover_tasks(['parsers.nba.tasks', 'parsers.nhl.tasks'])
 app.conf.beat_schedule = {
     'save-nba-standings-every-hour': {
         'task': 'parsers.nba.tasks.save_nba_standings',
-        'schedule': crontab(hour='*/1'),
+        'schedule': crontab(minute='0', hour='*/2'),
     },
     'save-nhl-standings-every-hour': {
         'task': 'parsers.nhl.tasks.save_nhl_standings',
-        'schedule': crontab(hour='*/1'),
+        'schedule': crontab(minute='0', hour='*/2'),
     },
     'parse-nba-games-every-hour': {
         'task': 'parsers.nba.tasks.parse_nba_games',
-        'schedule': crontab(hour='*/1'),
+        'schedule': crontab(minute='0', hour='*/2'),
     },
     'parse-nhl-games-every-hour': {
         'task': 'parsers.nhl.tasks.parse_nhl_games',
-        'schedule': crontab(hour='*/1'),
+        'schedule': crontab(minute='0', hour='*/1'),
     },
 }
