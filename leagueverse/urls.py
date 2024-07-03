b92336b9595e2fcca53a5264ba06d39119f4af0a
vlone <ermolich158@gmail.com>
2024-07-03 20:32:26 +0300

    add media url  to url patterns

--- leagueverse/urls.py
+++ leagueverse/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
@@ -24,3 +26,6 @@ urlpatterns = [
     path('api/v1/nhl/', include('nhl.urls')),
     path('api/v1/users/', include('users.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
